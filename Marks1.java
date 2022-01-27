2685f8cbe793ef05f0d78e15ae692e1ddf48bb80
[jacksonkgezane] <85163443+jacksonkgezane@users.noreply.github.com>
2022-01-27 11:46:48 +0200

    Update Marks1.java
    declaring and adding a scanner statement.

--- Marks1.java
+++ Marks1.java
@@ -5,6 +5,17 @@ class main {
     public static void main(String[] args) {
 
 
+        //declare variables
+        int mark;
+
+
+
+
+        //scanner statement
+        Scanner sc = new Scanner(System.in);
+
+
+
 
 
 
